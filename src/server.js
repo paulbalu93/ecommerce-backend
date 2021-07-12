b1cc936f87182364f2e38271072dfdff43ecc6dc
Paul Maliakel <paulbalu93@gmail.com>
2021-07-12 19:35:51 +0200

    initial commit

--- src/server.js
+++ src/server.js
@@ -1,3 +1,11 @@
 import express from 'express';
 
-const app = express();
+const server = express();
+
+server.get('/', (req, res, next) => {
+	res.status(200).send('hello');
+});
+
+server.listen(process.env.PORT, () => {
+	console.log(`Server is running on port ${process.env.PORT} `);
+});
